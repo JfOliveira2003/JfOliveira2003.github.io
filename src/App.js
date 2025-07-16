948bc3f8b05938ee1c0e9bbe521c2346bf2398a4
fabricio.jose <fabricio.jose@a4pm.com.br>
2025-07-16 16:04:33 -0300

    Adjusting centralization and adding more images

--- src/App.js
+++ src/App.js
@@ -22,47 +22,54 @@ function App() {
         <section id="experience">
           <h2>Experience</h2>
           <div className="card">
-            <div className="experience-box">
-              <h3>A4PM</h3>
-              <p>
-                <strong>DevOps Engineer</strong> | 2024 - Present
-              </p>
-              <p>
-                I am responsible for building and scaling software for
-                large-scale government applications with container orchestration
-                using Kubernetes and GitLab CI/CD pipelines.
-              </p>
-              <ul>
-                <li>Implemented CI/CD pipelines for various applications.</li>
-                <li>Managed cloud infrastructure on AWS.</li>
-                <li>Automated operational tasks using Python and Bash.</li>
-              </ul>
-            </div>
-            <div className="experience-box">
-              <h3>CEFET/RJ</h3>
-              <p>
-                <strong>Software Engineer</strong> | 2023 - 2024
-              </p>
-              <p>
-                My university raised a team to build a management system for
-                them, i was a software engineer responsible to help build the
-                backend using Laravel framework and Php. I had my first contact
-                with agile methodologies in this project.
+            <div className="experience-wrapper">
+              <div className="experience-box">
+                <h3 id="exp">
+                  A4PM <img src="/a4pm.png" alt="" width={25} height={25} />
+                </h3>
+
+                <p>
+                  <strong>DevOps Engineer</strong> | 2024 - Present
+                </p>
+                <p>
+                  I am responsible for building and scaling software for
+                  large-scale government applications with container
+                  orchestration using Kubernetes and GitLab CI/CD pipelines.
+                </p>
                 <ul>
-                  <li>
-                    Implemented the Business logical in the logical layer at the
-                    backend.
-                  </li>
-                  <li>
-                    Responsible to build the api endpoints in the presentation
-                    layer for the frontend{" "}
-                  </li>
-                  <li>
-                    Perfomed the documentation register for future workers
-                    understanding.
-                  </li>
+                  <li>Implemented CI/CD pipelines for various applications.</li>
+                  <li>Managed cloud infrastructure on AWS.</li>
+                  <li>Automated operational tasks using Python and Bash.</li>
                 </ul>
-              </p>
+              </div>
+              <div className="experience-box">
+                <h3 id="exp">
+                  CEFET/RJ <img src="/cefet.png" width={50} height={35} />
+                </h3>
+                <p>
+                  <strong>Software Engineer</strong> | 2023 - 2024
+                </p>
+                <p>
+                  My university raised a team to build a management system for
+                  them, i was a software engineer responsible to help build the
+                  backend using Laravel framework and Php. I had my first
+                  contact with agile methodologies in this project.
+                  <ul>
+                    <li>
+                      Implemented the Business logical in the logical layer at
+                      the backend.
+                    </li>
+                    <li>
+                      Responsible to build the api endpoints in the presentation
+                      layer for the frontend{" "}
+                    </li>
+                    <li>
+                      Perfomed the documentation register for future workers
+                      understanding.
+                    </li>
+                  </ul>
+                </p>
+              </div>
             </div>
           </div>
         </section>
@@ -137,32 +144,34 @@ function App() {
               </a>
             </div>
             <div className="project-box">
-              <h3>FLB - Fabricio Load Balancer</h3>
+              <img src="/loadbalancer.png"></img>
               <p>
                 Load Balancer Service written in the go programming language
               </p>
+              <a href="https://">Know more</a>
             </div>
           </div>
         </section>
-        <section id="contact">
-          <h2>Contact</h2>
-          <div className="card">
-            <p>Email: fabriciojosemacedo@gmail.com</p>
-            <a href="https://www.linkedin.com/in/fabricio-oliveira-a11a012b7">
-              {" "}
-              Linkedin
-            </a>
-            <a href="https://github.com/JfOliveira2003">
-              <img
-                width={50}
-                height={50}
-                src="github-142-svgrepo-com.svg"
-                alt=""
-              />
-            </a>
-          </div>
-        </section>
       </div>
+      <footer id="contact">
+        <div className="contact-box">
+          <a href="mailto:fabriciojosemacedo@gmail.com">
+            <img src="/gmail.svg" width={50} height={50} />
+          </a>
+          <a href="https://www.linkedin.com/in/fabricio-oliveira-a11a012b7">
+            {" "}
+            Linkedin
+          </a>
+          <a href="https://github.com/JfOliveira2003">
+            <img
+              width={50}
+              height={50}
+              src="github-142-svgrepo-com.svg"
+              alt=""
+            />
+          </a>
+        </div>
+      </footer>
     </div>
   );
 }
